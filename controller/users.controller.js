a1e9e440ded86772ba8b5dbcc5cf3ebe66845dcb
hsm990 <houssam.toumi44@gmail.com>
2026-04-20 21:19:31 +0100

    another cookies issue

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -94,7 +94,7 @@ const logInUser = asyncWrapper(async (req, res, next) => {
     res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production', // true in production only
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 24 * 60 * 60 * 1000 // 1 day
     })
     return res.json({
