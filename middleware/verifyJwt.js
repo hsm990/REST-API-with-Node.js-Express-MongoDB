e46949dc83613db8572953c0066eeb422a4269e3
hsm990 <houssam.toumi44@gmail.com>
2026-04-24 06:05:04 +0100

    fix verifyJwt middleware

--- middleware/verifyJwt.js
+++ middleware/verifyJwt.js
@@ -2,14 +2,14 @@ const jwt = require('jsonwebtoken')
 const AppError = require('../utils/appError')
 const httpStatus = require('../utils/httpStatus')
 module.exports = (req, res, next) => {
-    const headers = req.body.authorization
-    if (!headers?.startWiths("Bearer")) {
+    const headers = req.headers.authorization
+    if (!headers?.startsWith("Bearer")) {
         return next(new AppError('token is required', 401, httpStatus.FAIL))
     }
     const token = headers.split(" ")[1]
     try {
-        const decod = jwt.verify(token, process.env.JWT_SECRET_KEY)
-        req.user = decod
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+        req.user = decoded
         return next()
     }
     catch {
